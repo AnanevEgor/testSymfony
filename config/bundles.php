955487f06008d3e73ecabe2f4aecc33082d538ce
AnanevEgor <ananev.egor.96@gmail.com>
2018-11-27 19:18:26 +0400

    add get user

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,7 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -8,68 +8,82 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class UsersController extends Controller
+class UsersController extends FOSRestController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
 
     /**
-     * @Route("/users/{id}",name="update_user", defaults={"id"=""}))
-     * @Method("PATCH")
+     * UsersController constructor.
+     * @param EntityManagerInterface $em
      */
-    public function update($id){
-        if($id){
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
-        }else{
-            echo "передайте id юзера";
+    /**
+     * @Route("/users/{id}",name="update_user", defaults={"id"=""}, methods={"PATCH"})
+     *
+     */
+    public function update(Request $request, $id)
+    {
+        $view = $this->view("wfwe", 200);
+
+        return $view;
+    }
+
+    /**
+     * @Route("/users/{id}",name="info_user", defaults={"id"=""}, methods={"GET"})
+     */
+    public function info(Request $request, $id)
+    {
+        if ($id) {
+            $user = $this->em->getRepository(User::class)->find($id);
+            $view = $this->view($user, 200);
+            return $view;
+        }
+        return new Response();
+
+    }
+
+    /**
+     * @Route("/users/{id}",name="delete_user", defaults={"id"=""})
+     */
+    public function delete(Request $request, $id)
+    {
+        if ($id) {
         }
         return new Response();
     }
 
     /**
-     * @Route("/users/{id}",name="info_user", defaults={"id"=""}))
-     * @Method("GET")
+     * @Route("/users",name="add_user", methods={"POST"})
      */
-    public function info($id){
-        if($id){
-        }else{
-            echo "передайте id юзера";
-        }
-        return new Response();
+    public function add(Request $request)
+    {
 
-    }
-
-    /**
-     * @Route("/users/{id}",name="delete_user", defaults={"id"=""}))
-     * @Method("DELETE")
-     */
-    public function delete($id){
-        if($id){
-        }else{
-            echo "передайте id юзера";
-        }
         return new Response();
     }
 
     /**
-     * @Route("/users",name="add_user"))
-     * @Method("POST")
+     * @Route("/users",name="list_user", methods={"GET"})
      */
-    public function add(){
-        echo "передайте id юзера";
-        return new Response();
-    }
-
-    /**
-     * @Route("/users",name="list_user"))
-     * @Method("GET")
-     */
-    public function list(){
+    public function list(Request $request)
+    {
         return new Response();
     }
 }
